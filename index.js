2c743aa6737cfc7bd92e538e1bc8b661121c1b1e
[A[Balexia turner <alexiaturner411@gmail.com>
2022-08-02 01:41:02 -0700

    solved grade problem

--- index.js
+++ index.js
@@ -283,12 +283,20 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
-}
-
-
-
+function grade(number){
+  if(number >=90){
+    return 'you got an A';
+  }else if(number < 90 && number >= 80){
+    return 'you got a B';
+  }else if(number < 80 && number >= 70){
+  return 'you got a C';
+  }else if(number < 70 && number >= 60){
+    return 'you got a D';
+  }else if(number < 60)
+    return'you got an F';
+  }
+  
+  console.log(grade(100))
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
 
 //Vowel Counter - How many vowels are there?
