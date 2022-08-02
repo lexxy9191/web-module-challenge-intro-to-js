b48e38cb8c5ba7f00753b01d1873821fc7fa2052
[A[Balexia turner <alexiaturner411@gmail.com>
2022-08-02 01:01:38 -0700

    finished task 5

--- index.js
+++ index.js
@@ -212,8 +212,6 @@ function game(user, computer){
       return `you lose!`;
     }
   }
-}
-
 console.log(game())
 
 
@@ -228,10 +226,10 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(km){
+ return km * 0.621371;
 }
-
+console.log(miles(5));
 
 
 //Task 5b - Feet to CM
@@ -242,9 +240,10 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+return cm/30.48
 }
+console.log(feet(7))
 
 
 
